183e0043474cdf1a1aa3fb86cf5b84a5936c1489
Liortalus <liortalus@live.com>
2018-01-27 17:56:07 +0100

    Saving tokens to cookies, and headers of benches service

--- AngularApp/src/app/services/benches.service.ts
+++ AngularApp/src/app/services/benches.service.ts
@@ -7,24 +7,30 @@ import {Constants} from '../Constants';
 import {catchError, tap} from 'rxjs/operators';
 
 import { HandleErrorService} from './handle-error.service';
-
-/** constant for defining the content type */
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class BenchesService {
 
   private benchesUrl = Constants.API_URL + 'benches';  // URL to web api
+  private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
 
   constructor(
     private http: HttpClient,
-    public handleErrorService: HandleErrorService) { }
+    public handleErrorService: HandleErrorService,
+    private cookieService: CookieService) {
+    if (cookieService.check('UserAccessToken')) {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': 'bearer ' + this.cookieService.get('UserAccessToken')})
+      };
+    }
+  }
 
   /** GET benches from the server */
   getBenches(): Observable<Bench[]> {
-    return this.http.get<Bench[]>(this.benchesUrl, {responseType: 'json'})
+    return this.http.get<Bench[]>(this.benchesUrl, this.httpOptions)
       .pipe(
         tap(benches => this.handleErrorService.log(`BenchesService: fetched benches`)),
         catchError(this.handleErrorService.handleError('getBenches', []))
@@ -33,7 +39,7 @@ export class BenchesService {
 
   /** PUT: update the bench on the server */
   updateBench (bench: Bench): Observable<any> {
-    return this.http.put(this.benchesUrl + '/' + bench.id, bench, httpOptions).pipe(
+    return this.http.put(this.benchesUrl + '/' + bench.id, bench, this.httpOptions).pipe(
       tap(_ => this.handleErrorService.log(`updated bench id=${bench.id}`)),
       catchError(this.handleErrorService.handleError<any>('updateBench'))
     );
@@ -41,7 +47,7 @@ export class BenchesService {
 
   /** POST: add a new bench to the server */
   addBench (bench: Bench): Observable<Bench> {
-    return this.http.post<Bench>(this.benchesUrl + '/', bench, httpOptions).pipe(
+    return this.http.post<Bench>(this.benchesUrl + '/', bench, this.httpOptions).pipe(
       tap((bench1: Bench) => this.handleErrorService.log(`added bench`)),
       catchError(this.handleErrorService.handleError<Bench>('addBench'))
     );
@@ -52,7 +58,7 @@ export class BenchesService {
     const id = typeof bench === 'number' ? bench : bench.id;
     const url = `${this.benchesUrl}/${id}`;
 
-    return this.http.delete<Bench>(url, httpOptions).pipe(
+    return this.http.delete<Bench>(url, this.httpOptions).pipe(
       tap(_ => this.handleErrorService.log(`deleted bench id=${id}`)),
       catchError(this.handleErrorService.handleError<Bench>('deletedBench'))
     );
