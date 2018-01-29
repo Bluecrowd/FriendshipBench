7c05ef4800937722d676909eedef53d95ede83cf
Bas <b.d.r._bas@live.nl>
2018-01-30 00:34:29 +0100

    added questionnaire detail view

--- AngularApp/src/app/components/questionnaire/questionnaire.component.ts
+++ AngularApp/src/app/components/questionnaire/questionnaire.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Questionnaire} from '../../models/questionnaire';
+import {ActivatedRoute} from '@angular/router';
+import {QuestionnairesService} from '../../services/questionnaires.service';
 
 @Component({
   selector: 'app-questionnaire',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionnaire.component.css']
 })
 export class QuestionnaireComponent implements OnInit {
+  @Input() questionnaire: Questionnaire;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private questionnairesService: QuestionnairesService,
+  ) { }
 
   ngOnInit() {
+    this.getQuestionnaire();
+  }
+
+  getQuestionnaire(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.questionnairesService.getQuestionnaire(id)
+      .subscribe(questionnaire => this.questionnaire = questionnaire);
   }
 
 }
